2c2ae3b3dc8f981623cbe0c61f861c0207173ee4
gonzalezluizpro <gonzalezluizpro@gmail.com>
2024-11-27 21:21:56 +0100

    Add Google Analytics integration

---
+++ src/app/components/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+// src/app/components/GoogleAnalytics.tsx
+'use client';
+
+import Script from 'next/script'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16460762051"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16460762051');
+        `}
+      </Script>
+    </>
+  )
+}
